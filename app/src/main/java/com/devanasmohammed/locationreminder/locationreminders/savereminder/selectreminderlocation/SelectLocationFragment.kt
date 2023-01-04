d96d7954f05b2b29eb5d361c62b3a5872b216d67
dev-anasmohammed <anasmahammed5@gmail.com>
2023-01-04 18:44:30 +0200

    LocationPermissionHelper class to request and check permissions

--- app/src/main/java/com/devanasmohammed/locationreminder/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/devanasmohammed/locationreminder/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,18 +1,31 @@
 package com.devanasmohammed.locationreminder.locationreminders.savereminder.selectreminderlocation
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import com.devanasmohammed.locationreminder.R
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
+import com.devanasmohammed.locationreminder.BuildConfig
 import com.devanasmohammed.locationreminder.base.BaseFragment
 import com.devanasmohammed.locationreminder.databinding.FragmentSelectLocationBinding
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.model.*
 import com.devanasmohammed.locationreminder.locationreminders.savereminder.SaveReminderViewModel
+import com.devanasmohammed.locationreminder.utils.LocationPermissionHelper
 import com.devanasmohammed.locationreminder.utils.setDisplayHomeAsUpEnabled
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.material.snackbar.Snackbar
 import org.koin.android.ext.android.inject
 
 @Suppress("DEPRECATION")
@@ -22,6 +35,10 @@ class SelectLocationFragment : BaseFragment() , OnMapReadyCallback {
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
     private lateinit var map: GoogleMap
+    private lateinit var activityResultLauncherPermissions: ActivityResultLauncher<Array<String>>
+    private var isGetLocation = false
+//    private var marker: Marker? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -40,8 +57,29 @@ class SelectLocationFragment : BaseFragment() , OnMapReadyCallback {
             childFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-//        TODO: zoom to the user location after taking his permission
-//        TODO: add style to the map
+        activityResultLauncherPermissions =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
+                if (result.all { result -> result.value }) {
+                    //granted
+                    getUserLocation()
+                } else {
+                    //not granted
+                    Snackbar.make(
+                        requireView(),
+                        R.string.permission_denied_explanation, Snackbar.LENGTH_LONG
+                    )
+                        .setAction(R.string.settings) {
+                            // Displays App settings screen.
+                            startActivity(Intent().apply {
+                                action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                                data = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
+                                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            })
+                        }.show()
+                }
+            }
+
+
 //        TODO: put a marker to location that the user selected
 
 
@@ -49,6 +87,54 @@ class SelectLocationFragment : BaseFragment() , OnMapReadyCallback {
         onLocationSelected()
 
         return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //zoom to the user location after taking his permission
+        LocationPermissionHelper(
+            requireActivity(),requireView(),activityResultLauncherPermissions
+        ).checkPermissionThenDoMethod {
+            getUserLocation()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getUserLocation() {
+        if (!isGetLocation) {
+            val mLocationRequest: LocationRequest = LocationRequest.create()
+            mLocationRequest.interval = 1000
+            mLocationRequest.fastestInterval = 1000
+            mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            val mLocationCallback: LocationCallback = object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult) {
+                    for (location in locationResult.locations) {
+                        if (location != null && !isGetLocation) {
+                            val userLocation = LatLng(location.latitude, location.longitude)
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 25f))
+                            Log.e("location","${location.latitude} , ${location.longitude}")
+                        //                            marker = map.addMarker(
+//                                MarkerOptions().position(userLocation).title("Your Location")
+//                            )
+//                            marker?.showInfoWindow()
+//                            isGetLocation = true
+                        }
+                    }
+                }
+            }
+            LocationServices.getFusedLocationProviderClient(requireActivity())
+                .requestLocationUpdates(
+                    mLocationRequest,
+                    mLocationCallback,
+                    requireActivity().mainLooper
+                )
+
+            LocationServices.getFusedLocationProviderClient(requireActivity())
+                .lastLocation.addOnSuccessListener {}
+        }
+
     }
 
     private fun onLocationSelected() {
@@ -63,17 +149,21 @@ class SelectLocationFragment : BaseFragment() , OnMapReadyCallback {
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        // TODO: Change the map type based on the user's selection.
+        //Change the map type based on the user's selection.
         R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
             true
         }
         R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
             true
         }
         R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
             true
         }
         R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
             true
         }
         else -> super.onOptionsItemSelected(item)
