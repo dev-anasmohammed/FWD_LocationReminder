fbd8a5c5ab657b15f1d0399e59cb989424926111
dev-anasmohammed <anasmahammed5@gmail.com>
2023-01-04 21:45:44 +0200

    add checking device location settings

--- app/src/main/java/com/devanasmohammed/locationreminder/locationreminders/savereminder/SaveReminderFragment.kt
+++ app/src/main/java/com/devanasmohammed/locationreminder/locationreminders/savereminder/SaveReminderFragment.kt
@@ -102,10 +102,6 @@ class SaveReminderFragment : BaseFragment() {
                     checkLocationSettingsAndStartGeofence()
                 }
             }
-
-//            TODO: use the user entered reminder details to:
-//             1) add a geofencing request
-//             2) save the reminder to the local db
         }
     }
 
