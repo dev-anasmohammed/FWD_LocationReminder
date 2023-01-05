fea5afe4fe9d5b5437fd75cc8bc5160a840e9d46
dev-anasmohammed <anasmahammed5@gmail.com>
2023-01-05 19:10:23 +0200

    test RemindersActivity

--- app/src/androidTest/java/com/devanasmohammed/locationreminder/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/devanasmohammed/locationreminder/RemindersActivityTest.kt
@@ -1,16 +1,31 @@
 package com.devanasmohammed.locationreminder
 
+import android.app.Activity
 import android.app.Application
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.devanasmohammed.locationreminder.locationreminders.RemindersActivity
 import com.devanasmohammed.locationreminder.locationreminders.data.ReminderDataSource
 import com.devanasmohammed.locationreminder.locationreminders.data.local.LocalDB
 import com.devanasmohammed.locationreminder.locationreminders.data.local.RemindersLocalRepository
 import com.devanasmohammed.locationreminder.locationreminders.reminderslist.RemindersListViewModel
 import com.devanasmohammed.locationreminder.locationreminders.savereminder.SaveReminderViewModel
+import com.devanasmohammed.locationreminder.util.DataBindingIdlingResource
+import com.devanasmohammed.locationreminder.util.monitorActivity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.core.IsNot.not
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -22,10 +37,20 @@ import org.koin.test.get
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 //END TO END test to black box test the app
-class RemindersActivityTest : AutoCloseKoinTest() {// Extended Koin Test - embed autoclose @after method to close Koin after every test
+class RemindersActivityTest : AutoCloseKoinTest() {
 
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    // get activity context
+    private fun getActivity(activityScenario: ActivityScenario<RemindersActivity>): Activity? {
+        var activity: Activity? = null
+        activityScenario.onActivity {
+            activity = it
+        }
+        return activity
+    }
 
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
@@ -48,7 +73,7 @@ class RemindersActivityTest : AutoCloseKoinTest() {// Extended Koin Test - embed
                     get() as ReminderDataSource
                 )
             }
-            single { RemindersLocalRepository(get()) as ReminderDataSource }
+            single { RemindersLocalRepository(get()) }
             single { LocalDB.createRemindersDao(appContext) }
         }
         //declare a new koin module
@@ -64,6 +89,100 @@ class RemindersActivityTest : AutoCloseKoinTest() {// Extended Koin Test - embed
         }
     }
 
-//    TODO: add End to End testing to the app
+    /**
+     * Testing add reminder by selecting location
+     * and enter title and description
+     */
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun addReminder() = runTest {
+
+        //start up tasks screen.
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        //click on add reminder button
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        //go to select location
+        onView(withId(R.id.selectLocation)).perform(click())
+
+        //add marker on map, and save location
+        onView(withContentDescription("Google Map")).perform(longClick())
+        onView(withId(R.id.save_btn)).perform(click())
+
+        //add title and description
+        onView(withId(R.id.reminderTitle)).perform(replaceText("Shop"))
+        onView(withId(R.id.reminderDescription)).perform(replaceText("buy products from the shop"))
+
+        //click on save reminder
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        //testing saving toast
+        onView(withText(R.string.reminder_saved))
+            .inRoot(withDecorView(not(`is`(getActivity(activityScenario)?.window?.decorView))))
+            .check(matches(isDisplayed()))
+
+        //verify reminder is displayed on screen in the reminder list.
+        onView(withText("Shop")).check(matches(isDisplayed()))
+
+        //close activity resetting the db.
+        activityScenario.close()
+    }
+
+    /**
+     * Test error enter title
+     */
+    @Test
+    fun showSnackBar_errorEnterTitle(){
+        //start up tasks screen.
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        //click on fab to add reminder
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        //click on save reminder
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        //check that snack bar shows error
+        onView(withId(com.google.android.material.R.id.snackbar_text))
+            .check(matches(withText(R.string.err_enter_title)))
+
+        //close activity resetting the db.
+        activityScenario.close()
+
+    }
+
+    /**
+     * Test error select location
+     */
+    @Test
+    fun showSnackBar_errorEnterLocation(){
+        //start up tasks screen.
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        //click on fab to add reminder
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        //click on edittext to enter title of reminder
+        onView(withId(R.id.reminderTitle)).perform(typeText("Shop"), closeSoftKeyboard())
+
+        //click on edittext to enter title of description
+        onView(withId(R.id.reminderDescription)).perform(typeText("buy prodcuts from the shop"), closeSoftKeyboard())
+
+        //click on save reminder
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        //check that snack bar shows error
+        onView(withId(com.google.android.material.R.id.snackbar_text))
+            .check(matches(withText(R.string.err_select_location)))
+
+        //close activity resetting the db.
+        activityScenario.close()
+    }
+
+
 
 }
--- app/src/androidTest/java/com/devanasmohammed/locationreminder/util/DataBindingIdlingResource.kt
+++ app/src/androidTest/java/com/devanasmohammed/locationreminder/util/DataBindingIdlingResource.kt
@@ -86,12 +86,12 @@ fun DataBindingIdlingResource.monitorActivity(
     }
 }
 
-///**
-// * Sets the fragment from a [FragmentScenario] to be used from [DataBindingIdlingResource].
-// */
-//fun DataBindingIdlingResource.monitorFragment(fragmentScenario: FragmentScenario<out Fragment>) {
-//    fragmentScenario.onFragment {
-//        this.activity = it.requireActivity()
-//    }
-//}
+/**
+ * Sets the fragment from a [FragmentScenario] to be used from [DataBindingIdlingResource].
+ */
+fun <T : Fragment> DataBindingIdlingResource.monitorFragment(fragmentScenario: FragmentScenario<T>) {
+    fragmentScenario.onFragment {
+        this.activity = it.requireActivity()
+    }
+}
 
